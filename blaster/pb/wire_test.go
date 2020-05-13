f1f8f15b348a578043653e7442ccd09e02d6858e
Michael Quigley <michael@quigley.com>
2020-05-13 19:23:44 -0400

    Testing.

--- blaster/pb/wire_test.go
+++ blaster/pb/wire_test.go
@@ -2,19 +2,21 @@ package pb
 
 import (
 	"bytes"
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestReadWrite(t *testing.T) {
+	wmIn := NewHello(9, "OH, WOW")
+
 	buffer := new(bytes.Buffer)
-	err := WriteMessage(NewHello(9, "OH, WOW"), buffer)
-	if err != nil {
-		panic(err)
-	}
-	wm, err := ReadMessage(buffer)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(wm.String())
+
+	err := WriteMessage(wmIn, buffer)
+	assert.Nil(t, err)
+
+	wmOut, err := ReadMessage(buffer)
+	assert.Nil(t, err)
+
+	assert.Equal(t, wmIn.Type, wmOut.Type)
+	assert.Equal(t, wmIn.HelloPayload.Session, wmOut.HelloPayload.Session)
 }
--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/cobra v1.0.0
+	github.com/stretchr/testify v1.5.1
 	google.golang.org/protobuf v1.22.0
 )
