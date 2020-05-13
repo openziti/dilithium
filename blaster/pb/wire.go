791bd2fe021e6ba22c9591a93b79db0b1a8892d0
Michael Quigley <michael@quigley.com>
2020-05-13 16:13:31 -0400

    PB elaboration. (#15)

--- blaster/pb/wire.go
+++ blaster/pb/wire.go
@@ -1,16 +1,23 @@
 package pb
 
+import (
+	"bytes"
+	"encoding/binary"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
+)
+
 func NewSync(sequence int32) *WireMessage {
 	return &WireMessage{
 		Sequence: sequence,
-		Type: MessageType_SYNC,
+		Type:     MessageType_SYNC,
 	}
 }
 
 func NewHello(sequence int32, session string) *WireMessage {
 	return &WireMessage{
 		Sequence: sequence,
-		Type: MessageType_HELLO,
+		Type:     MessageType_HELLO,
 		HelloPayload: &WireMessage_HelloPayload{
 			Session: session,
 		},
@@ -20,6 +27,28 @@ func NewHello(sequence int32, session string) *WireMessage {
 func NewOk(sequence int32) *WireMessage {
 	return &WireMessage{
 		Sequence: sequence,
-		Type: MessageType_OK,
+		Type:     MessageType_OK,
 	}
+}
+
+func ToData(wireMessage *WireMessage) ([]byte, error) {
+	pbData, err := proto.Marshal(wireMessage)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal")
+	}
+
+	data := make([]byte, len(pbData)+4)
+	buffer := bytes.NewBuffer(data)
+	if err := binary.Write(buffer, binary.LittleEndian, len(pbData)); err != nil {
+		return nil, errors.Wrap(err, "write length")
+	}
+	n, err := buffer.Write(pbData)
+	if err != nil {
+		return nil, errors.Wrap(err, "write pb data")
+	}
+	if n != len(pbData) {
+		return nil, errors.New("short pb data")
+	}
+
+	return data, nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/emirpasic/gods v1.12.0
+	github.com/golang/protobuf v1.4.0
 	github.com/google/uuid v1.1.1
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
@@ -11,4 +12,5 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/cobra v1.0.0
+	google.golang.org/protobuf v1.22.0
 )
