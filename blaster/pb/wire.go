47fd47d50757eb4b5fe02b8de3e652ead525bd7e
Michael Quigley <michael@quigley.com>
2020-05-13 16:04:47 -0400

    Convience methods to construct ProtoBufs wire messages. (#15)

--- blaster/pb/wire.go
+++ blaster/pb/wire.go
@@ -1,5 +1,25 @@
 package pb
 
-func NewSync() {
+func NewSync(sequence int32) *WireMessage {
+	return &WireMessage{
+		Sequence: sequence,
+		Type: MessageType_SYNC,
+	}
+}
 
+func NewHello(sequence int32, session string) *WireMessage {
+	return &WireMessage{
+		Sequence: sequence,
+		Type: MessageType_HELLO,
+		HelloPayload: &WireMessage_HelloPayload{
+			Session: session,
+		},
+	}
+}
+
+func NewOk(sequence int32) *WireMessage {
+	return &WireMessage{
+		Sequence: sequence,
+		Type: MessageType_OK,
+	}
 }
