cbdc9162bce01153d69c0694c4410fb4a826642b
Michael Quigley <michael@quigley.com>
2020-05-18 13:51:58 -0400

    Still more westworld listener elaboration. (#16)

--- protocol/westworld/listenerconn.go
+++ protocol/westworld/listenerconn.go
@@ -1,8 +1,60 @@
 package westworld
 
-import "github.com/michaelquigley/dilithium/protocol/westworld/pb"
+import (
+	"github.com/michaelquigley/dilithium/protocol/westworld/pb"
+	"github.com/michaelquigley/dilithium/util"
+	"net"
+	"time"
+)
 
-type listenerConn struct{}
+type listenerConn struct {
+	conn    *net.UDPConn
+	peer    *net.UDPAddr
+	rxQueue chan *pb.WireMessage
+	seq     *util.Sequence
+}
+
+func newListenerConn(conn *net.UDPConn, peer *net.UDPAddr) *listenerConn {
+	return &listenerConn{
+		conn:    conn,
+		peer:    peer,
+		rxQueue: make(chan *pb.WireMessage, 1024),
+		seq:     util.NewSequence(),
+	}
+}
+
+func (self *listenerConn) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (self *listenerConn) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func (self *listenerConn) Close() error {
+	return nil
+}
+
+func (self *listenerConn) RemoteAddr() net.Addr {
+	return self.peer
+}
+
+func (self *listenerConn) LocalAddr() net.Addr {
+	return self.conn.LocalAddr()
+}
+
+func (self *listenerConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (self *listenerConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (self *listenerConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
 
 func (self *listenerConn) queue(wm *pb.WireMessage) {
+	self.rxQueue <- wm
 }
