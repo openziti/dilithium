004e14228de592be377e99ebfde8113d5bc5aa21
Michael Quigley <michael@quigley.com>
2020-05-29 16:34:08 -0400

    Basic lockless txWindow with rx/tx for acks, tx for wireMessage with ack sliplining. Pooled. (#24)

--- protocol/westworld2/txwindow.go
+++ protocol/westworld2/txwindow.go
@@ -3,6 +3,7 @@ package westworld2
 import (
 	"github.com/emirpasic/gods/trees/btree"
 	"github.com/emirpasic/gods/utils"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"net"
 )
@@ -12,7 +13,8 @@ type txWindow struct {
 	capacity int
 	txQueue  chan *wireMessage
 	txErrors chan error
-	ackQueue chan int32
+	rxAcks   chan int32
+	txAcks   chan int32
 	conn     *net.UDPConn
 	peer     *net.UDPAddr
 	pool     *pool
@@ -24,7 +26,8 @@ func newTxWindow(ackQueue chan int32, conn *net.UDPConn, peer *net.UDPAddr) *txW
 		capacity: startingWindowCapacity,
 		txQueue:  make(chan *wireMessage, txQueueSize),
 		txErrors: make(chan error, txErrorsSize),
-		ackQueue: make(chan int32, ackQueueSize),
+		rxAcks:   make(chan int32, ackQueueSize),
+		txAcks:   make(chan int32, ackQueueSize),
 		conn:     conn,
 		peer:     peer,
 		pool:     newPool("txWindow"),
@@ -47,16 +50,53 @@ func (self *txWindow) run() {
 				self.capacity--
 
 				select {
-				case ack, ok := <-self.ackQueue:
+				case ack, ok := <-self.txAcks:
 					if !ok {
 						return
 					}
 					wm.rewriteAck(ack)
 				default:
+					// no ack for slipstream
 				}
 
+				if err := writeWireMessage(wm, self.conn, self.peer); err != nil {
+					logrus.Errorf("-> {#%d,@%d}[%d] -> (%v)", wm.seq, wm.ack, len(wm.data), err)
+					self.txErrors <- errors.Wrap(err, "write")
+				}
+
+			case txAck, ok := <-self.txAcks:
+				if !ok {
+					return
+				}
+
+				wm := newAck(txAck, self.pool.get())
+				if err := writeWireMessage(wm, self.conn, self.peer); err != nil {
+					logrus.Errorf("{@%d} -> (%v)", txAck, err)
+				}
+				wm.buffer.unref()
+
 			default:
+				// no wireMessage for tx
 			}
+		}
+		
+		select {
+		case rxAck, ok := <- self.rxAcks:
+			if !ok {
+				return
+			}
+
+			if wm, found := self.tree.Get(rxAck); found {
+				self.tree.Remove(rxAck)
+				wm.(*wireMessage).buffer.unref()
+				self.capacity++
+
+			} else {
+				logrus.Warnf("~ [@%d] <-", rxAck) // already acked
+			}
+
+		default:
+			// no acks to process
 		}
 	}
 }
