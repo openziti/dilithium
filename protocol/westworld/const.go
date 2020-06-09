a65a770abdbb15d9ec4ce1b9c75879c338457635
Michael Quigley <michael@quigley.com>
2020-06-09 14:21:34 -0400

    Removed old 'westworld' implementation.

--- protocol/westworld/const.go
+++
@@ -1,7 +0,0 @@
-package westworld
-
-const startingTreeSize = 1024
-const startingWindowCapacity = 8
-const queueLength = 1024
-const retransmissionDelayMs = 10
-const rxtxBufferSize = 128 * 1024 * 1024
