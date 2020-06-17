1721262dbe7433040a05df4c709a96b64a668051
Michael Quigley <michael@quigley.com>
2020-06-17 12:15:23 -0400

    This needs to be configurable.

--- protocol/westworld2/const.go
+++ protocol/westworld2/const.go
@@ -1,9 +1,9 @@
 package westworld2
 
 const (
-	startingWindowCapacity = 4 * 1024
+	startingWindowCapacity = 3 * 1024
 	mss                    = 1500
-	retxTimeoutMs          = 25
+	retxTimeoutMs          = 100
 	treeSz                 = 1024
 	readsSz                = 1024
 	listenerRxQueueSz      = 1024
