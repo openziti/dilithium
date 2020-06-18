b024faf924c95292731ffb0b74dedf898f250c35
Michael Quigley <michael@quigley.com>
2020-06-18 14:52:58 -0400

    Configs naming. (#32)

--- protocol/westworld2/config.go
+++ protocol/westworld2/config.go
@@ -6,31 +6,38 @@ import (
 )
 
 type Config struct {
-	portalStartSz int
-	mss           int
-	retxTimeoutMs int
-	treeSz        int
-	readsQSz      int
-	listenerRxQSz int
-	acceptQSz     int
-	bufferSz      int
-	i             Instrument
+	poolBufferSz   int
+	portalStartSz  int
+	maxSegmentSz   int
+	retxTimeoutMs  int
+	treeLen        int
+	readsQLen      int
+	listenerRxQLen int
+	acceptQLen     int
+	i              Instrument
 }
 
 func NewDefaultConfig() *Config {
 	return &Config{
-		portalStartSz: 3 * 1024,
-		mss:           1500,
-		retxTimeoutMs: 100,
-		treeSz:        1024,
-		readsQSz:      1024,
-		listenerRxQSz: 1024,
-		acceptQSz:     1024,
-		bufferSz:      64 * 1024,
+		poolBufferSz:   64 * 1024,
+		portalStartSz:  3 * 1024,
+		maxSegmentSz:   1500,
+		retxTimeoutMs:  100,
+		treeLen:        1024,
+		readsQLen:      1024,
+		listenerRxQLen: 1024,
+		acceptQLen:     1024,
 	}
 }
 
 func (self *Config) Load(data map[interface{}]interface{}) error {
+	if v, found := data["pool_buffer_sz"]; found {
+		if i, ok := v.(int); ok {
+			self.poolBufferSz = i
+		} else {
+			return errors.New("invalid 'pool_buffer_sz' value")
+		}
+	}
 	if v, found := data["portal_start_sz"]; found {
 		if i, ok := v.(int); ok {
 			self.portalStartSz = i
@@ -38,11 +45,11 @@ func (self *Config) Load(data map[interface{}]interface{}) error {
 			return errors.New("invalid 'portal_start_sz' value")
 		}
 	}
-	if v, found := data["mss"]; found {
+	if v, found := data["max_segment_sz"]; found {
 		if i, ok := v.(int); ok {
-			self.mss = i
+			self.maxSegmentSz = i
 		} else {
-			return errors.New("invalid 'mss' value")
+			return errors.New("invalid 'max_segment_sz' value")
 		}
 	}
 	if v, found := data["retx_timeout_ms"]; found {
@@ -52,39 +59,32 @@ func (self *Config) Load(data map[interface{}]interface{}) error {
 			return errors.New("invalid 'retx_timeout_ms' value")
 		}
 	}
-	if v, found := data["tree_sz"]; found {
+	if v, found := data["tree_len"]; found {
 		if i, ok := v.(int); ok {
-			self.treeSz = i
+			self.treeLen = i
 		} else {
-			return errors.New("invalid 'tree_sz' value")
+			return errors.New("invalid 'tree_len' value")
 		}
 	}
-	if v, found := data["reads_q_sz"]; found {
+	if v, found := data["reads_q_len"]; found {
 		if i, ok := v.(int); ok {
-			self.readsQSz = i
+			self.readsQLen = i
 		} else {
-			return errors.New("invalid 'reads_q_sz' value")
+			return errors.New("invalid 'reads_q_len' value")
 		}
 	}
-	if v, found := data["listener_rx_q_sz"]; found {
+	if v, found := data["listener_rx_q_len"]; found {
 		if i, ok := v.(int); ok {
-			self.listenerRxQSz = i
+			self.listenerRxQLen = i
 		} else {
-			return errors.New("invalid 'listener_rx_q_sz' value")
+			return errors.New("invalid 'listener_rx_q_len' value")
 		}
 	}
-	if v, found := data["accept_q_sz"]; found {
+	if v, found := data["accept_q_len"]; found {
 		if i, ok := v.(int); ok {
-			self.acceptQSz = i
+			self.acceptQLen = i
 		} else {
-			return errors.New("invalid 'accept_q_sz' value")
-		}
-	}
-	if v, found := data["buffer_sz"]; found {
-		if i, ok := v.(int); ok {
-			self.bufferSz = i
-		} else {
-			return errors.New("invalid 'buffer_sz' value")
+			return errors.New("invalid 'accept_q_len' value")
 		}
 	}
 	return nil
@@ -92,14 +92,14 @@ func (self *Config) Load(data map[interface{}]interface{}) error {
 
 func (self *Config) Dump() string {
 	out := "westworld2.Config{\n"
-	out += fmt.Sprintf("\t%-20s %d\n", "portalStartSz", self.portalStartSz)
-	out += fmt.Sprintf("\t%-20s %d\n", "mss", self.mss)
-	out += fmt.Sprintf("\t%-20s %d\n", "retxTimeoutMs", self.retxTimeoutMs)
-	out += fmt.Sprintf("\t%-20s %d\n", "treeSz", self.treeSz)
-	out += fmt.Sprintf("\t%-20s %d\n", "readsQSz", self.readsQSz)
-	out += fmt.Sprintf("\t%-20s %d\n", "listenerRxQSz", self.listenerRxQSz)
-	out += fmt.Sprintf("\t%-20s %d\n", "acceptQSz", self.acceptQSz)
-	out += fmt.Sprintf("\t%-20s %d\n", "bufferSz", self.bufferSz)
+	out += fmt.Sprintf("\t%-20s %d\n", "pool_buffer_sz", self.poolBufferSz)
+	out += fmt.Sprintf("\t%-20s %d\n", "portal_start_sz", self.portalStartSz)
+	out += fmt.Sprintf("\t%-20s %d\n", "max_segment_sz", self.maxSegmentSz)
+	out += fmt.Sprintf("\t%-20s %d\n", "retx_timeout_ms", self.retxTimeoutMs)
+	out += fmt.Sprintf("\t%-20s %d\n", "tree_len", self.treeLen)
+	out += fmt.Sprintf("\t%-20s %d\n", "reads_q_len", self.readsQLen)
+	out += fmt.Sprintf("\t%-20s %d\n", "listener_rx_q_len", self.listenerRxQLen)
+	out += fmt.Sprintf("\t%-20s %d\n", "accept_q_len", self.acceptQLen)
 	out += "}"
 	return out
 }
