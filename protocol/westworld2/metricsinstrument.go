dc561ff93b2f5f712252dbc03cade8b8ce38083d
Michael Quigley <michael@quigley.com>
2020-06-29 15:14:31 -0400

    Basic metrics output. (#37)

--- protocol/westworld2/metricsinstrument.go
+++ protocol/westworld2/metricsinstrument.go
@@ -1,9 +1,13 @@
 package westworld2
 
 import (
+	"fmt"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net"
+	"os"
+	"path/filepath"
 	"sync"
 	"time"
 )
@@ -11,28 +15,19 @@ import (
 type metricsInstrument struct {
 	prefix           string
 	lock             *sync.Mutex
-	txBytes          samples
-	retxBytes        samples
-	rxBytes          samples
-	txPortalSz       samples
-	duplicateRxBytes samples
-	duplicateAcks    samples
-	rextMs           samples
-	allocations      samples
+	txBytes          []*sample
+	retxBytes        []*sample
+	rxBytes          []*sample
+	txPortalSz       []*sample
+	duplicateRxBytes []*sample
+	duplicateAcks    []*sample
+	rextMs           []*sample
+	allocations      []*sample
 }
 
 type sample struct {
 	ts time.Time
 	v  int64
-}
-
-type samples []*sample
-
-func (self samples) last() *sample {
-	if len(self) > 0 {
-		return self[len(self)-1]
-	}
-	return nil
 }
 
 func newMetricsInstrument(config map[string]interface{}) (Instrument, error) {
@@ -49,7 +44,20 @@ func (self *metricsInstrument) connected(_ *net.UDPAddr) {
 }
 
 func (self *metricsInstrument) closed(_ *net.UDPAddr) {
-	logrus.Infof("writing metrics to prefix [%s]", self.prefix)
+	if err := os.MkdirAll(self.prefix, os.ModePerm); err == nil {
+		outPath, err := ioutil.TempDir(self.prefix, "")
+		if err == nil {
+			logrus.Infof("writing metrics to prefix [%s]", outPath)
+			if err := self.writeSamples("txPortalSz", outPath, self.txPortalSz); err != nil {
+				logrus.Errorf("error writing txPortalSz (%v)", err)
+				return
+			}
+		} else {
+			logrus.Errorf("error writing metrics (%v)", err)
+		}
+	} else {
+		logrus.Errorf("unable to make output parent [%s] (%v)", self.prefix, err)
+	}
 }
 
 func (self *metricsInstrument) wireMessageRx(_ *net.UDPAddr, wm *wireMessage) {
@@ -120,6 +128,17 @@ func (self *metricsInstrument) configure(data map[string]interface{}) error {
 		} else {
 			return errors.New("invalid 'prefix' type")
 		}
+	}
+	return nil
+}
+
+func (self *metricsInstrument) writeSamples(name, outPath string, samples []*sample) error {
+	out := fmt.Sprintf("ts,%s\n", name)
+	for _, sample := range samples {
+		out += fmt.Sprintf("%d,%d\n", sample.ts.UnixNano(), sample.v)
+	}
+	if err := ioutil.WriteFile(filepath.Join(outPath, fmt.Sprintf("%s.csv", name)), []byte(out), os.ModePerm); err != nil {
+		return errors.Wrap(err, "write metrics")
 	}
 	return nil
 }
