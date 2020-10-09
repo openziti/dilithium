59def0002f39c97dd5bb6815f34784cc321bffb7
Michael Quigley <michael@quigley.com>
2020-10-09 15:38:17 -0400

    Support multiple loop metrics instances. (#59)

--- protocol/loop/metrics.go
+++ protocol/loop/metrics.go
@@ -1,12 +1,22 @@
 package loop
 
 import (
+	"fmt"
 	"github.com/michaelquigley/dilithium/util"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
+
+func init() {
+	go signalHandler()
+}
 
 type Metrics struct {
 	Addr   *net.TCPAddr
@@ -27,6 +37,9 @@ func NewMetrics(addr, peer *net.TCPAddr, ms int, prefix string) *Metrics {
 		Prefix: prefix,
 		close:  make(chan struct{}, 1),
 	}
+	registryLock.Lock()
+	registry = append(registry, m)
+	registryLock.Unlock()
 	go m.snapshotter(ms)
 	return m
 }
@@ -41,6 +54,16 @@ func (self *Metrics) Tx(bytes int64) {
 
 func (self *Metrics) Close() {
 	close(self.close)
+	registryLock.Lock()
+	j := 0
+	for _, m := range registry {
+		if m == self {
+			break
+		}
+		j++
+	}
+	registry = append(registry[:j], registry[j+1:]...)
+	registryLock.Unlock()
 }
 
 func (self *Metrics) snapshotter(ms int) {
@@ -55,6 +78,47 @@ func (self *Metrics) snapshotter(ms int) {
 			return
 		default:
 			//
+		}
+	}
+}
+
+func (self *Metrics) writeAllSamples() error {
+	if err := os.MkdirAll(self.Prefix, os.ModePerm); err != nil {
+		return err
+	}
+	name := fmt.Sprintf("%s_%s", self.Addr, self.Peer)
+	outPath, err := ioutil.TempDir(self.Prefix, name)
+	if err != nil {
+		return err
+	}
+	logrus.Infof("writing metrics to: %s", outPath)
+
+	if err := util.WriteSamples("rxBytes", outPath, self.RxBytes); err != nil {
+		return err
+	}
+	if err := util.WriteSamples("txBytes", outPath, self.TxBytes); err != nil {
+		return err
+	}
+	return nil
+}
+
+var registry []*Metrics
+var registryLock sync.Mutex
+
+func signalHandler() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGUSR2)
+
+	for {
+		s := <-c
+		if s == syscall.SIGUSR2 {
+			registryLock.Lock()
+			for _, m := range registry {
+				if err := m.writeAllSamples(); err != nil {
+					logrus.Errorf("error writing samples (%v)", err)
+				}
+			}
+			registryLock.Unlock()
 		}
 	}
 }
