52494f909bd0ceb8892de62e0043333e8b32ec67
Michael Quigley <michael@quigley.com>
2020-10-13 15:48:05 -0400

    Basic 2-byte timestamp testing for westworld3. (#58)

---
+++ cmd/ditests/tbts.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func testTwoByteTimestamps() {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 1024; i++ {
+		sourceTs := uint16(time.Now().UnixNano() / int64(time.Millisecond))
+		randMs := rand.Intn(1024)
+		time.Sleep(time.Duration(randMs) * time.Millisecond)
+		checkTs := uint16(time.Now().UnixNano() / int64(time.Millisecond))
+		delta := checkTs - sourceTs
+		if int(delta) < (randMs - int(float64(randMs) * 0.05)) || int(delta) > (randMs + int(float64(randMs) * 0.05)) {
+			fmt.Printf("{%d != %d}", delta, randMs)
+		} else {
+			fmt.Printf(".")
+		}
+	}
+	fmt.Println()
+}
