b5962a0bfa6945ff58b8f85379773c65a7b5e94c
Michael Quigley <michael@quigley.com>
2020-10-14 12:55:52 -0400

    ACK data will be framed in the data section, paving the way for bulk ACK support. (#58, #74)

--- protocol/westworld3/message.go
+++ protocol/westworld3/message.go
@@ -1,8 +1,15 @@
 package westworld3
 
+import (
+	"github.com/michaelquigley/dilithium/util"
+	"github.com/pkg/errors"
+)
+
 type wireMessage struct {
-	seq int32
-	mt  messageType
+	seq    int32
+	mt     messageType
+	data   []byte
+	buffer *buffer
 }
 
 type messageType uint8
@@ -21,3 +28,27 @@ const (
 	// 0x8 ... 0x80
 	RTT messageFlag = 0x8
 )
+
+const headerSz = 7
+
+func (self *wireMessage) encode() *wireMessage {
+	util.WriteInt32(self.buffer.data[0:4], self.seq)
+	self.buffer.data[4] = byte(self.mt)
+	util.WriteUint16(self.buffer.data[5:headerSz], uint16(len(self.data)))
+	self.buffer.sz = uint32(headerSz + len(self.data))
+	return self
+}
+
+func decode(buffer *buffer) (*wireMessage, error) {
+	sz := util.ReadUint16(buffer.data[5:headerSz])
+	if uint32(headerSz+sz) > buffer.sz {
+		return nil, errors.Errorf("short buffer read [%d != %d]", buffer.sz, headerSz+sz)
+	}
+	wm := &wireMessage{
+		seq:    util.ReadInt32(buffer.data[0:4]),
+		mt:     messageType(buffer.data[4]),
+		data:   buffer.data[headerSz : headerSz+sz],
+		buffer: buffer,
+	}
+	return wm, nil
+}
