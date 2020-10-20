a13ce94f38f9532417922ae45de7d7284aaefb69
Michael Quigley <michael@quigley.com>
2020-10-20 14:15:51 -0400

    The westworld3 starting baseline profile (#74)

--- protocol/westworld3/profile.go
+++ protocol/westworld3/profile.go
@@ -26,3 +26,31 @@ type Profile struct {
 	acceptQueueLen         int
 	i                      Instrument
 }
+
+func NewBaselineProfile() *Profile {
+	return &Profile{
+		seqRandom:              true,
+		txPortalStartSz:        16 * 1024,
+		txPortalMinSz:          16 * 1024,
+		txPortalMaxSz:          4 * 1024 * 1024,
+		txPortalIncreaseThresh: 224,
+		txPortalIncreaseScale:  1.0,
+		txPortalDupAckThresh:   64,
+		txPortalDupAckScale:    0.9,
+		txPortalRetxThresh:     64,
+		txPortalRetxScale:      0.75,
+		retxStartMs:            200,
+		retxScale:              2.0,
+		retxAddMs:              100,
+		rttProbeMs:             50,
+		rttProbeAvg:            8,
+		maxSegmentSz:           1420,
+		poolBufferSz:           64 * 1024,
+		rxBufferSz:             16 * 1024 * 1024,
+		txBufferSz:             16 * 1024 * 1024,
+		treeLen:                1024,
+		readsQueueLen:          1024,
+		listenerRxQueueLen:     1024,
+		acceptQueueLen:         1024,
+	}
+}
