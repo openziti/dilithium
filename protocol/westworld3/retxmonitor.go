5a6df6a890a2a37cf6039514e6ff95d10788bc22
Michael Quigley <michael@quigley.com>
2020-10-22 14:47:57 -0400

    Primitive Add comparison between arrayWaitlist.Add and btreeWaitlist.Add (#77)

--- protocol/westworld3/retxmonitor.go
+++ protocol/westworld3/retxmonitor.go
@@ -1,7 +1,8 @@
 package westworld3
 
 import (
-	"github.com/pkg/errors"
+	"github.com/emirpasic/gods/trees/btree"
+	"github.com/emirpasic/gods/utils"
 	"sync"
 	"time"
 )
@@ -27,11 +28,6 @@ type arrayWaitlist struct {
 }
 
 func (self *arrayWaitlist) Add(wm *wireMessage, t time.Time) error {
-	if len(self.waitlist) > 0 {
-		if t.Before(self.waitlist[len(self.waitlist)-1].deadline) {
-			return errors.New("violated ordering constraint")
-		}
-	}
 	self.waitlist = append(self.waitlist, &retxSubject{t, wm})
 	return nil
 }
@@ -55,4 +51,44 @@ func (self *arrayWaitlist) Next() (*wireMessage, time.Time) {
 	next := self.waitlist[0]
 	self.waitlist = self.waitlist[1:]
 	return next.wm, next.deadline
+}
+
+type btreeWaitlist struct {
+	waitlist *btree.Tree
+}
+
+func newBtreeWaitlist() *btreeWaitlist {
+	return &btreeWaitlist{waitlist: btree.NewWith(1024, utils.TimeComparator)}
+}
+
+func (self *btreeWaitlist) Add(wm *wireMessage, t time.Time) error {
+	if v, found := self.waitlist.Get(t); found {
+		switch v.(type) {
+		case *wireMessage:
+			vAsserted := v.(*wireMessage)
+			if vAsserted != wm {
+				tl := btree.NewWith(128, wireMessageComparator)
+				tl.Put(vAsserted, nil)
+				tl.Put(wm, nil)
+				self.waitlist.Put(t, tl)
+			}
+
+		case *btree.Tree:
+			vAsserted := v.(*btree.Tree)
+			vAsserted.Put(wm, nil)
+		}
+	}
+	return nil
+}
+
+func wireMessageComparator(a interface{}, b interface{}) int {
+	aAsserted := a.(*wireMessage)
+	bAsserted := b.(*wireMessage)
+	if aAsserted.seq > bAsserted.seq {
+		return 1
+	} else if aAsserted.seq < bAsserted.seq {
+		return -1
+	} else {
+		return 0
+	}
 }
--- protocol/westworld3/retxmonitor_test.go
+++ protocol/westworld3/retxmonitor_test.go
@@ -5,18 +5,32 @@ import (
 	"time"
 )
 
-func BenchmarkArrayWaitlist_Add(b *testing.B) {
-	aw := &arrayWaitlist{}
-	var toAdd []*retxSubject
+var waitlistBenchmarkToAdd []*retxSubject
+func init() {
 	for i := 0; i < 1024; i++ {
-		toAdd = append(toAdd, &retxSubject{time.Now().Add(200 * time.Millisecond), &wireMessage{seq: int32(i)}})
+		waitlistBenchmarkToAdd = append(waitlistBenchmarkToAdd, &retxSubject{time.Now().Add(200 * time.Millisecond), &wireMessage{seq: int32(i)}})
 		time.Sleep(1 * time.Millisecond)
 	}
+}
 
+func BenchmarkArrayWaitlist_Add(b *testing.B) {
+	aw := &arrayWaitlist{}
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		for i := 0; i < 1024; i++ {
-			if err := aw.Add(toAdd[i].wm, toAdd[i].deadline); err != nil {
+			if err := aw.Add(waitlistBenchmarkToAdd[i].wm, waitlistBenchmarkToAdd[i].deadline); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func BenchmarkBtreeWaitlist_Add(b *testing.B) {
+	btw := newBtreeWaitlist()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < 1024; i++ {
+			if err := btw.Add(waitlistBenchmarkToAdd[i].wm, waitlistBenchmarkToAdd[i].deadline); err != nil {
 				panic(err)
 			}
 		}
