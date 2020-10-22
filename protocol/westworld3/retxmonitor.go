4700a78d0c7c12a6f5583eb34b8c2b66cab94c79
Michael Quigley <michael@quigley.com>
2020-10-22 14:13:59 -0400

    Effectively the naive westworld2 implementation, for benchmarking. (#77)

--- protocol/westworld3/retxmonitor.go
+++ protocol/westworld3/retxmonitor.go
@@ -1,14 +1,20 @@
 package westworld3
 
 import (
-	"github.com/emirpasic/gods/trees/btree"
+	"github.com/pkg/errors"
 	"sync"
 	"time"
 )
 
 type retxMonitor struct {
-	waiting *btree.Tree
-	ready   *sync.Cond
+	waitlist *waitlist
+	ready    *sync.Cond
+}
+
+type waitlist interface {
+	Add(*wireMessage, time.Time)
+	Remove(*wireMessage)
+	Next() (*wireMessage, time.Time, bool)
 }
 
 type retxSubject struct {
@@ -16,9 +22,37 @@ type retxSubject struct {
 	wm       *wireMessage
 }
 
-type waitlist interface {
-	Add(*wireMessage, time.Time)
-	Remove(*wireMessage)
-	Next(retxSubject) (time.Time, bool)
-	All() []*wireMessage
+type arrayWaitlist struct {
+	waitlist []*retxSubject
+}
+
+func (self *arrayWaitlist) Add(wm *wireMessage, t time.Time) error {
+	if len(self.waitlist) > 0 {
+		if t.Before(self.waitlist[len(self.waitlist)-1].deadline) {
+			return errors.New("violated ordering constraint")
+		}
+	}
+	self.waitlist = append(self.waitlist, &retxSubject{t, wm})
+	return nil
+}
+
+func (self *arrayWaitlist) Remove(wm *wireMessage) {
+	i := -1
+	for i = 0; i < len(self.waitlist); i++ {
+		if self.waitlist[i].wm == wm {
+			break
+		}
+	}
+	if i > -1 {
+		self.waitlist = append(self.waitlist[:i], self.waitlist[i+1:]...)
+	}
+}
+
+func (self *arrayWaitlist) Next() (*wireMessage, time.Time) {
+	if len(self.waitlist) < 1 {
+		return nil, time.Time{}
+	}
+	next := self.waitlist[0]
+	self.waitlist = self.waitlist[1:]
+	return next.wm, next.deadline
 }
