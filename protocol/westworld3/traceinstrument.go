51dd10409b70a1ea1a60da203e6c87407bf7cab5
Michael Quigley <michael@quigley.com>
2020-11-11 14:54:54 -0500

    Protocol decoding. (#85)

--- protocol/westworld3/traceinstrument.go
+++ protocol/westworld3/traceinstrument.go
@@ -3,6 +3,7 @@ package westworld3
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 type traceInstrument struct{}
@@ -45,15 +46,18 @@ func (self *traceInstrumentInstance) Closed(peer *net.UDPAddr) {
  * wire
  */
 func (self *traceInstrumentInstance) WireMessageTx(peer *net.UDPAddr, wm *wireMessage) {
-	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s}", self.id, "TX", wm.seq, wm.messageType(), wm.mt.FlagsString())
+	decode, _ := self.decode(wm)
+	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s} -> %s", self.id, "TX", wm.seq, wm.messageType(), wm.mt.FlagsString(), decode)
 }
 
 func (self *traceInstrumentInstance) WireMessageRetx(peer *net.UDPAddr, wm *wireMessage) {
-	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s}", self.id, "RETX", wm.seq, wm.messageType(), wm.mt.FlagsString())
+	decode, _ := self.decode(wm)
+	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s} -> %s", self.id, "RETX", wm.seq, wm.messageType(), wm.mt.FlagsString(), decode)
 }
 
 func (self *traceInstrumentInstance) WireMessageRx(peer *net.UDPAddr, wm *wireMessage) {
-	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s}", self.id, "RX", wm.seq, wm.messageType(), wm.mt.FlagsString())
+	decode, _ := self.decode(wm)
+	self.queue <- fmt.Sprintf("&& %-24s %-8s #%-8d %s {%s} -> %s", self.id, "RX", wm.seq, wm.messageType(), wm.mt.FlagsString(), decode)
 }
 
 func (self *traceInstrumentInstance) UnknownPeer(peer *net.UDPAddr) {
@@ -115,4 +119,45 @@ func (self *traceInstrumentInstance) run() {
 			fmt.Println(l)
 		}
 	}
+}
+
+func (self *traceInstrumentInstance) decode(wm *wireMessage) (string, error) {
+	out := ""
+	switch wm.messageType() {
+	case HELLO:
+		h, acks, err := wm.asHello()
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("{v:%d, p:%d} |%s|", h.version, h.profile, self.decodeAcks(acks)), nil
+
+	case DATA:
+		sz, err := wm.asDataSize()
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf(":%d", sz), nil
+
+	case ACK:
+		a, rxPortalSz, _, err := wm.asAck()
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("|%s| %%%d", self.decodeAcks(a), rxPortalSz), nil
+
+	default:
+		return out, nil
+	}
+}
+
+func (self *traceInstrumentInstance) decodeAcks(acks []ack) string {
+	out := ""
+	for _, ack := range acks {
+		if ack.start == ack.end {
+			out += fmt.Sprintf(" @%d", ack.start)
+		} else {
+			out += fmt.Sprintf(" @%d:%d", ack.start, ack.end)
+		}
+	}
+	return strings.TrimSpace(out)
 }
