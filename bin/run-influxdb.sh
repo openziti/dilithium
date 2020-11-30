9fe95b4a62f61985436853966bcbc921fc81ad4a
Michael Quigley <michael@quigley.com>
2020-11-30 13:22:34 -0500

    Better local path for influxdb script.

--- bin/run-influxdb.sh
+++ bin/run-influxdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8086:8086 --name=influxdb -v /opt/influxdb:/var/lib/influxdb influxdb
+docker run -d -p 8086:8086 --name=influxdb -v /opt/influxdb/var/lib/influxdb:/var/lib/influxdb influxdb
