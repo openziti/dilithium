db2350d3617a7523a84a9278a9cf2ecd110e03ac
Michael Quigley <michael@quigley.com>
2020-12-02 11:48:39 -0500

    SIGQUIT -> goroutine dumps.

--- cmd/dilithium/main.go
+++ cmd/dilithium/main.go
@@ -8,6 +8,11 @@ import (
 	_ "github.com/openziti/dilithium/cmd/dilithium/loop"
 	_ "github.com/openziti/dilithium/cmd/dilithium/tunnel"
 	"github.com/sirupsen/logrus"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 )
 
 func init() {
@@ -17,6 +22,17 @@ func init() {
 
 func main() {
 	defer logrus.Infof("finished")
+
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGQUIT)
+		buf := make([]byte, 1<<20)
+		for {
+			<-sigs
+			stacklen := runtime.Stack(buf, true)
+			log.Printf("=== received SIGQUIT ===\n*** goroutine dump...\n%s\n*** end\n", buf[:stacklen])
+		}
+	}()
 
 	if err := dilithium.RootCmd.Execute(); err != nil {
 		logrus.Fatalf("error (%v)", err)
