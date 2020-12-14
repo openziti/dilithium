3e0728c426d73a576ccbf7c5a55696c3490a9173
Michael Quigley <michael@quigley.com>
2020-12-14 12:15:40 -0500

    Strawman westlsworld3 factory. (#97)

--- cmd/dilithium/dilithium/factory.go
+++ cmd/dilithium/dilithium/factory.go
@@ -2,22 +2,17 @@ package dilithium
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"github.com/lucas-clemente/quic-go"
 	"github.com/openziti/dilithium/cf"
+	"github.com/openziti/dilithium/protocol/westlsworld3"
 	"github.com/openziti/dilithium/protocol/westworld2"
 	"github.com/openziti/dilithium/protocol/westworld3"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"math/big"
 	"net"
-	"time"
 )
 
 type Protocol interface {
@@ -203,37 +198,57 @@ func ProtocolFor(protocol string) (Protocol, error) {
 		}
 		return impl, nil
 
+	case "westlsworld3":
+		p := westworld3.NewBaselineProfile()
+		if configPath != "" {
+			if configPath != "" {
+				data, err := ioutil.ReadFile(configPath)
+				if err != nil {
+					return nil, errors.Wrapf(err, "unable to read config file [%s]", configPath)
+				}
+				dataMap := make(map[interface{}]interface{})
+				if err = yaml.Unmarshal(data, dataMap); err != nil {
+					return nil, errors.Wrapf(err, "unable to unmarshal config data [%s]", configPath)
+				}
+				if err = p.Load(cf.MapIToMapS(dataMap)); err != nil {
+					return nil, errors.Wrapf(err, "unable to load westworld3 profile [%s]", configPath)
+				}
+			}
+		}
+		profileId, err := westworld3.AddProfile(p)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to register westworld3 profile")
+		}
+		if configDump {
+			logrus.Infof(p.Dump())
+		}
+
+		impl := struct{ ProtoProtocol }{}
+		impl.listen = func(address string) (Accepter, error) {
+			listenAddress, err := net.ResolveUDPAddr("udp", address)
+			if err != nil {
+				return nil, errors.Wrap(err, "resolve address")
+			}
+			listener, err := westlsworld3.Listen(listenAddress, generateTLSConfig(), profileId)
+			if err != nil {
+				return nil, errors.Wrap(err, "listen")
+			}
+			return listener, nil
+		}
+		impl.dial = func(address string) (net.Conn, error) {
+			dialAddress, err := net.ResolveUDPAddr("udp", address)
+			if err != nil {
+				return nil, errors.Wrap(err, "resolve address")
+			}
+			conn, err := westlsworld3.Dial(dialAddress, generateTLSConfig(), profileId)
+			if err != nil {
+				return nil, errors.Wrap(err, "dial")
+			}
+			return conn, nil
+		}
+		return impl, nil
+
 	default:
 		return nil, errors.Errorf("unsupported protocol [%s]", protocol)
-	}
-}
-
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{
-		SerialNumber: big.NewInt(1),
-		NotBefore:    time.Now().Add(-24 * time.Hour),
-		NotAfter:     time.Now().Add(24 * time.Hour),
-		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1)},
-		IsCA:         true,
-	}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates:       []tls.Certificate{tlsCert},
-		NextProtos:         []string{"dilithium"},
-		InsecureSkipVerify: true,
 	}
 }
---
+++ cmd/dilithium/dilithium/tls.go
@@ -0,0 +1,42 @@
+package dilithium
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"time"
+)
+
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    time.Now().Add(-24 * time.Hour),
+		NotAfter:     time.Now().Add(24 * time.Hour),
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1)},
+		IsCA:         true,
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates:       []tls.Certificate{tlsCert},
+		NextProtos:         []string{"dilithium"},
+		InsecureSkipVerify: true,
+	}
+}
