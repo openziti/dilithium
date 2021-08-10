cc67919e9310a6819924044ebcc1d6f8c853df08
Michael Quigley <michael@quigley.com>
2021-08-10 16:37:15 -0400

    Baseline west profile elaboration.

--- west.go
+++ west.go
@@ -19,7 +19,33 @@ func (self *WestAlgorithm) Retransmission() {
 }
 
 type WestProfile struct {
-	TxPortalStartSize int
-	TxPortalMinSize   int
-	TxPortalMaxSize   int
+	StartSize           int
+	MinSize             int
+	MaxSize             int
+	SuccessThresh       int
+	SuccessScale        float64
+	DupAckThresh        int
+	DupAckCapacityScale float64
+	DupAckSuccessScale  float64
+	RetxThresh          int
+	RetxCapacityScale   float64
+	RetxSuccessScale    float64
+	RxSizePressureScale float64
+}
+
+func NewBaselineWestProfile() *WestProfile {
+	return &WestProfile{
+		StartSize:           96 * 1024,
+		MinSize:             16 * 1024,
+		MaxSize:             4 * 1024 * 1024,
+		SuccessThresh:       224,
+		SuccessScale:        1.0,
+		DupAckThresh:        64,
+		DupAckCapacityScale: 0.9,
+		DupAckSuccessScale:  0.75,
+		RetxThresh:          64,
+		RetxCapacityScale:   0.75,
+		RetxSuccessScale:    0.825,
+		RxSizePressureScale: 2.8911,
+	}
 }
